2e5974c7e76d90727d558dc5988c187f895e7e3b
Crashbandioof <40924574+Crashbandioof@users.noreply.github.com>
2024-04-19 21:23:39 -0500

    Update television.py

--- television.py
+++ television.py
@@ -9,26 +9,49 @@ class Television:
         self.volume = self.MIN_VOLUME
         self.channel = self.MIN_CHANNEL
     def power(self):
+        '''
+        Turns the TV on if the TV is off
+        Turns the TV off if the TV is on
+        '''
         if self.status == True:
             self.status = False
         else:
             self.status = True
     def mute(self):
+        '''
+        Mutes the TV if it isn't muted
+        Unmutes the TV if it's muted
+        '''
         if self.muted == True:
             self.muted = False
         else:
             self.muted = True
     def channel_up(self):
+        '''
+        Increases the channel by one if the channel isn't at the max channel
+        '''
         if self.channel < self.MAX_CHANNEL:
             self.channel += 1
     def channel_down(self):
+        '''
+        Decreases the channel by one if the channel isn't at the min channel
+        '''
         if self.channel > self.MIN_CHANNEL:
             self.channel -= 1
     def volume_up(self):
+        '''
+        Increases the volume by one if the volume isn't at the max volume
+        '''
         if self.volume < self.MAX_VOLUME:
             self.volume += 1
     def volume_down(self):
+        '''
+        Decreases the volume by one if the volume isn't at the min volume
+        '''
         if self.volume > self.MIN_VOLUME:
             self.volume -= 1
     def __str__(self):
+        '''
+        Returns the power status, channel, and volume
+        '''
         return f'Power = {self.status}, Channel = {self.channel}, Volume = {self.volume}'
