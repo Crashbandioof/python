9b43c61092cbeb0340fb9a3e4c6e73c94c7ee192
Crashbandioof <40924574+Crashbandioof@users.noreply.github.com>
2024-04-21 16:21:36 -0500

    S

--- test_television.py
+++ test_television.py
@@ -1,13 +1,2 @@
-import unittest
-from television import *
-class Mytestclass(unittest.TestCase):
-    def test_power(self):
-        tv_1 = Television()
-        self.assertEqual(tv_1.power(), True)
-        self.assertEqual(tv_1.power().power(), False)
-    def test_mute(self):
-        tv_1 = Television()
-        self.assertEqual(tv_1.mute(), True)
-        self.assertEqual(tv_1.mute().mute(), False)
-
-
+import pytest
+from television.py import *
